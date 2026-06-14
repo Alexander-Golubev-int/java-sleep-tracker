a2826b36cbcbbe99e0e00b01695130bd0968109a
Alexander Golubev <a.golubevspb1608@gmail.com>
2026-06-14 17:33:03 +0300

    fix code style errors

--- src/main/java/function/AmountOfSleepSessions.java
+++ src/main/java/function/AmountOfSleepSessions.java
@@ -5,7 +5,7 @@ import ru.yandex.practicum.sleeptracker.SleepingSession;
 import java.util.List;
 import java.util.function.Function;
 
-public class AmountOfSleepSessions implements Function <List<SleepingSession>,Integer>{
+public class AmountOfSleepSessions implements Function<List<SleepingSession>,Integer> {
 
    @Override
    public Integer apply(List<SleepingSession> sleepingSessionList) {
